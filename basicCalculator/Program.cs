45794c3bd558bf4e90060e7cbba547e69c29910f
Bastien <bastien.deresteau@gmail.com>
2020-11-09 12:09:08 +0100

    very very very basic calculator in c#

---
+++ basicCalculator/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Runtime.ExceptionServices;
+
+namespace basicCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write(" enter your first number : ");
+            int first = Int32.Parse(Console.ReadLine());
+            Console.Write(" enter your second number : ");
+            int second = Int32.Parse(Console.ReadLine());
+            int result = first + second;
+            Console.WriteLine(first + "+" + second + "=" + result);
+            result = first - second;
+            Console.WriteLine(first + " - " + second + " = " + result);
+            result = first / second;
+            Console.WriteLine(first + " / " + second + " = " + result);
+            result = first * second;
+            Console.WriteLine(first + " * " + second + " = " + result);
+
+
+        }
+    }
+}
